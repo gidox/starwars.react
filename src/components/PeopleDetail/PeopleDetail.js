8bee5a59e51bc27cdfe90e898b3206261c476fc4
Arquimedes <guilandarquimedes@gmail.com>
2017-08-19 12:50:57 -0500

    added moment

--- src/components/PeopleDetail/PeopleDetail.js
+++ src/components/PeopleDetail/PeopleDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import peoplesApi from 'api/starwars';
 import { Grid, Row, Col, Panel } from 'react-bootstrap';
 import { Link } from 'react-router';
-
+import Moment from 'react-moment';
 
 class PeopleDetail extends Component {
   constructor(props) {
@@ -39,7 +39,7 @@ class PeopleDetail extends Component {
                 <Col sm={3}>      
                 </Col>
                 <Col sm={6}>
-                  <h1>{this.state.people.name} {this.state.people.created}</h1>  
+                  <h1>{this.state.people.name} <small className="pull-right"><Moment format="YYYY/MM/DD">{this.state.people.created}</Moment></small></h1>  
                   <Panel>
                     <p><b>Gender:</b> {this.state.people.gender}</p>
                     <p><b>Hair color:</b> {this.state.people.hair_rolor}</p>
